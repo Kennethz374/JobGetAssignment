8a159c423745c5ec6aab8d26f82c5e6fbbdae38f
Kenneth <sally@sallyde-MacBook-Air.local>
2019-12-17 02:53:55 -0800

    call it for today

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {AppBar,Card, Paper} from '@material-ui/core';
+import { Paper} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 
